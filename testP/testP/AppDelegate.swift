83228e3219a2854a6b84ef4ca9598837fcd0f891
dmitrycvetkov2000 <dimacvetkov2000@mail.ru>
2023-09-12 17:13:25 +0300

    Initial project setup

---
+++ testP/testP/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  testP
+//
+//  Created by Дмитрий Цветков on 12.09.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow()
+        let vc = ViewController()
+        window.rootViewController = vc
+        window.makeKeyAndVisible()
+        self.window = window
+        return true
+    }
+}
+
---
+++ testP/testP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testP
+//
+//  Created by Дмитрий Цветков on 12.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+
+
+}
+
