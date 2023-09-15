5afc25ca5711ba86603d8bd8931f121fe1e3d845
dmitrycvetkov2000 <dimacvetkov2000@mail.ru>
2023-09-15 15:14:07 +0300

    set View model

--- testP/testP/FirstModule/View/FirstVC.swift
+++ testP/testP/FirstModule/View/FirstVC.swift
@@ -6,28 +6,47 @@
 //
 
 import UIKit
+import CoreData
 
 protocol FirstVCProtocol: AnyObject {
     var viewModel: FirstViewModelProtocol! { get set }
     var coordinator: AppCoordinator! { get set }
+    var helper: HelperForTableView! { get set }
 }
 
 class FirstVC: UIViewController, FirstVCProtocol {
     var viewModel: FirstViewModelProtocol!
     var coordinator: AppCoordinator!
+    var helper: HelperForTableView!
+    
+    private let tableView: UITableView = UITableView()
         
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .brown
-        viewModel.getListMovie {
-            print()
+        self.viewModel.getListMovie {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
         }
-        viewModel.getDetailInfoAboutMovie {
-            print()
-        }
+        createTableView()
     }
-
-
 }
 
+extension FirstVC {
+    func createTableView() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            tableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 0),
+            tableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
+        ])
+        
+        tableView.dataSource = helper
+        tableView.register(CustomTableViewCell.self, forCellReuseIdentifier: CustomTableViewCell.identificator)
+    }
+}
 
--- testP/testP/FirstModule/ViewModel/FirstViewModel.swift
+++ testP/testP/FirstModule/ViewModel/FirstViewModel.swift
@@ -6,25 +6,46 @@
 //
 
 import Foundation
+import CoreData
+import UIKit
 
 protocol FirstViewModelProtocol {
-    func getListMovie(completion: () -> Void)
+    var model: MovieModel! { get set }
+    func getListMovie(completion: @escaping () -> Void)
     func getDetailInfoAboutMovie(completion: () -> Void)
 }
 
-class FirstViewModel: FirstViewModelProtocol {
-    func getListMovie(completion: () -> Void) {
+class FirstViewModel {
+    var model: MovieModel!
+}
+
+extension FirstViewModel: FirstViewModelProtocol {
+
+    
+    func getListMovie(completion: @escaping () -> Void) {
         APIManager.shared.getListOfMovie { [weak self] movie in
             guard let self = self else { return }
-            
-
+            if movie != nil {
+                CoreDataManager.shared.deleteAllEntity(entityName: AllEnums.entityNames(name: .MovieEntity))
+                movie?.results?.forEach({ res in
+                    self.model!.results.append(SomeMovie(posterPath: "https://image.tmdb.org/t/p/w500/\(res.posterPath ?? "")", originalTitle: res.originalTitle ?? "" ))
+                })
+                completion()
+            } else {
+                let masEntity = CoreDataManager.shared.getAllEntity(entityName: AllEnums.entityNames(name: .MovieEntity))
+                let photoMas: [UIImage] = CoreDataManager.shared.getElementFromBD(findKey: AllEnums.keys(key: .photo))
+                let titleMas: [String] = CoreDataManager.shared.getElementFromBD(findKey: AllEnums.keys(key: .title))
+                for i in 0..<masEntity.count {
+                    self.model!.results.append(SomeMovie(posterPath: photoMas[i], originalTitle: titleMas[i] ))
+                }
+                completion()
+            }
         }
     }
     
     func getDetailInfoAboutMovie(completion: () -> Void) {
         APIManager.shared.getDetailInfoAboutMovie(id: 157336) { [weak self] detail in
             guard let self = self else { return }
-            
         }
     }
 }
